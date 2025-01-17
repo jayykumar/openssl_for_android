c88e036b642f730e585e569dc87fee0cc6ee3494
jayykumar <63598833+jayykumar@users.noreply.github.com>
2025-01-17 10:40:51 -0600

    Update openssl_build.sh

--- openssl_build.sh
+++ openssl_build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 WORK_PATH=$(cd "$(dirname "$0")";pwd)
-ANDROID_NDK_PATH=${WORK_PATH}/android-ndk-r23b
+ANDROID_NDK_PATH=${WORK_PATH}/android-ndk-r23c
 OPENSSL_SOURCES_PATH=${WORK_PATH}/openssl-1.1.1s
 ANDROID_TARGET_API=$1
 ANDROID_TARGET_ABI=$2
